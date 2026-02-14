df8c37c2bf0e8d88a38b6032122256177c588961
pritamawatade <pritamawatade5@gmail.com>
2026-02-14 17:47:50 +0530

    testing actions

--- index.test.js
+++ index.test.js
@@ -11,6 +11,6 @@ describe('Test the root path', () => {
     it("GET /:id shuld return 'Hello World! Your id is 123'", async () => {
         const res = await request(app).get('/123')
         expect(res.statusCode).toEqual(200)
-        expect(res.text).toEqual('Hello World! Your id is 123')
+        expect(res.text).toEqual('Hello World Your id is 123')
     })
 })
